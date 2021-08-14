898b962417e62a81c4859be2745ddab0b4b6d23e
Hrithik Babbar <hrithikbabbar22@gmail.com>
2021-08-14 14:52:32 +0530

    onload problem

--- public/sock.js
+++ public/sock.js
@@ -1,12 +1,14 @@
 const socket = io.connect();
 const d = new Date();
 const str = `${d}`
-function example() {
+window.onload = function example() {
     const temp_name = str;
+    console.log("1st" + str);
     socket.emit('new-user-joined', temp_name);
 }
 socket.on('user-joined', temp_name => {
     if (temp_name != str) {
+        console.log("2nd " + temp_name);
         window.location.reload();
     }
 })
