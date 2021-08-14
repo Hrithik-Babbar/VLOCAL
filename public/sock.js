513ed7cae0297eb259adb9b08ab360928ce9a50c
Hrithik Babbar <hrithikbabbar22@gmail.com>
2021-08-14 13:47:06 +0530

    temp

--- public/sock.js
+++ public/sock.js
@@ -3,9 +3,12 @@ const d = new Date();
 const str = `${d}`
 window.onload = function example() {
     const temp_name = str;
+    console.log("1st" + str);
     socket.emit('new-user-joined', temp_name);
 }
 socket.on('user-joined', temp_name => {
-    if (temp_name != str)
+    if (temp_name != str) {
+        console.log("2nd" + str);
         window.location.reload();
+    }
 })
